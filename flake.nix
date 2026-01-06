9a80a4a4b34d87521d5945eb385db79398a424e5
Luca Wellmeier <159539641+lcwllmr@users.noreply.github.com>
2026-01-06 11:08:40 +0100

    chore: add package to flake

--- flake.nix
+++ flake.nix
@@ -3,13 +3,25 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs = inputs:
-  let
-    pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
-  in {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        pnpm
-      ];
+    let
+      pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          pnpm
+        ];
+      };
+      packages.x86_64-linux.default = let
+        pname = "nanokvm-usb";
+        version = "0.1.0";
+        src = pkgs.fetchurl {
+          url = "https://github.com/lcwllmr/nanokvm-usb/releases/download/v${version}/nanokvm-usb-v${version}.AppImage";
+          hash = "sha256-Kz8Wx8ACzzNCVDKrXX44I6tSqErLOdZvNLsqjBuerO8=";
+          # hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        };
+      in
+        pkgs.appimageTools.wrapType2 {
+          inherit pname version src;
+        };
     };
-  };
 }
